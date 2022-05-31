8f71374f763076b95680c1ac3d1930f453084716
Raúl Granados <raul.granados12@gmail.com>
2022-05-30 22:32:34 -0600

    Deleting mongo db

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,6 +1,3 @@
-//const { MongoClient } = require("mongodb");
-const Mongoose = require("mongoose");
-
 const dbhost = process.env.DBHOST || "localhost";
 const dbport = process.env.DBPORT || "27017";
 const dbname = process.env.DBNAME;
@@ -9,12 +6,7 @@ const uri = process.env.DBURI || `mongodb://${dbhost}:${dbport}/${dbname}`
 
 const connect = async () => {
     try {
-        await Mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        });
+
         console.log("Connection succesfull");
     } catch (error) {
         console.log("Connection failed", error);
@@ -27,10 +19,9 @@ const connect = async () => {
  */
 const disconnect = async () => {
     try {
-        await Mongoose.disconnect();
-        console.log("Database disconnected");
+
     } catch (error) {
-        process.exit(1);
+
     }
 }
 
--- src/services/error.service.js
+++ src/services/error.service.js
@@ -1,19 +1,12 @@
 const ServiceResponse = require("../responses/ServiceResponse");
 const Errors = require('../errors');
-const mongoose = require("mongoose");
 const errorModel = require('../models/error.model');
 
 const errorService = {}
 
 //TODO: Change error attributes as needed
 errorService.save = async (errorCode, description, statusCode) => {
-    const newError = new errorModel({
-        errorCode,
-        description,
-        statusCode
-    });
 
-    await newError.save();
 }
 
 

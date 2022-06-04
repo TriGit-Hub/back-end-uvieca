fd5ec513b45d22b763282a04697d2f3beecb647e
Raúl Granados <raul.granados12@gmail.com>
2022-06-04 00:29:49 -0600

    modelo electricista

--- src/models/electricista.model.js
+++ src/models/electricista.model.js
@@ -2,9 +2,27 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const electricista = (sequelize) => {
     const electricista = sequelize.define('electricista', {
-            img: {
+            carnet: {
                 type: DataTypes.STRING,
-            }
+            },
+            dui: {
+                type: DataTypes.STRING,
+            },
+            email: {
+                type: DataTypes.STRING,
+            },
+            nit: {
+                type: DataTypes.STRING,
+            },
+            nombre: {
+                type: DataTypes.STRING,
+            },
+            registro: {
+                type: DataTypes.STRING,
+            },
+            telefono: {
+                type: DataTypes.STRING,
+            },
         },
         {
             freezeTableName: true

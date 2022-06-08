a3b70fff35a80835646181ac82a909062877588e
Raúl Granados <raul.granados12@gmail.com>
2022-06-07 18:43:23 -0600

    Creando router electricista

---
+++ src/routes/electricista.router.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var {runValidation} = require('../middlewares/express-validatorConf');
+var {registerValidator, loginValidator} = require('../validators/user.validator');
+
+var electricistaController = require("../controllers/cliente.controller");
+
+//CREATE
+router.post("/crear", electricistaController.save);
+
+//READ
+//router.get("/todos", electricistaController.findAll);
+
+//UPDATE
+
+//DELETE
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 var userRouter = require("./users.route");
 var authRouter = require("./auth.router");
-var clienteRouter = require("./cliente.router")
+var clienteRouter = require("./cliente.router");
+var electricistaController = require("./electricista.router");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,5 +15,6 @@ router.get('/', function(req, res, next) {
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
 router.use("/cliente", clienteRouter);
+router.use("/electricista", electricistaController);
 
 module.exports = router;

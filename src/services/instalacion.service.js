11b5e0fdbfc53ccdf5e035bb4d639c08f7ab787f
ragranados <raul.granados12@gmail.com>
2022-06-10 19:38:34 -0600

    Quitando transaccion a metodo para insertar instalacion

--- src/services/instalacion.service.js
+++ src/services/instalacion.service.js
@@ -9,24 +9,20 @@ const instalacionService = {}
 instalacionService.save = async (capacidad_subestacion, conexion_subestacion, capacidad_generador, carga_a_solicitar, clasificacion_instalacion,
                                  nombre_pro, nro_hojas, nro_niveles, nro_servicio_a_instalar, nro_transformadores, nro_tablero, tension_suministro, tipo_servicio) => {
 
-    const result = await db.sequelize.transaction(async (t) => {
-
-        const newInstalacion = await db.Instalacion.create({
-            capacidad_subestacion,
-            conexion_subestacion,
-            capacidad_generador,
-            carga_a_solicitar,
-            clasificacion_instalacion,
-            nombre_pro,
-            nro_hojas,
-            nro_niveles,
-            nro_servicio_a_instalar,
-            nro_transformadores,
-            nro_tablero,
-            tension_suministro,
-            tipo_servicio,
-        }, {transaction: t});
-
+    const newInstalacion = await db.Instalacion.create({
+        capacidad_subestacion,
+        conexion_subestacion,
+        capacidad_generador,
+        carga_a_solicitar,
+        clasificacion_instalacion,
+        nombre_pro,
+        nro_hojas,
+        nro_niveles,
+        nro_servicio_a_instalar,
+        nro_transformadores,
+        nro_tablero,
+        tension_suministro,
+        tipo_servicio,
     });
 
     return ServiceResponse(true, null);

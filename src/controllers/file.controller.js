6ca5eabf48f78aa313ad8a750ade899948ce4202
ragranados <raul.granados12@gmail.com>
2023-02-19 21:42:04 -0600

    Controlador para llenar PDF

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,13 +1,16 @@
 const ApiResponse = require("../responses/ApiResponse");
 const DTO = require('../dto');
 const Services = require('../services/index');
+const { PDFDocument } = require('pdf-lib');
+const fs = require('fs');
+
 var path = require('path');
 
-const electricistaController = {}
+const file = {}
 
-electricistaController.getImg = async (req, res, next) => {
+file.getImg = async (req, res, next) => {
     try {
-        const { imagen } = req.params;
+        const {imagen} = req.params;
 
         return res.sendFile(path.join(__dirname, `/../../public/uploads/img/${imagen}`));
 
@@ -17,4 +20,29 @@ electricistaController.getImg = async (req, res, next) => {
     }
 };
 
-module.exports = electricistaController;
+file.obtenerPdfSolicitud = async (req, res, next) => {
+    try {
+
+        const url = path.join(__dirname, `/../../public/OoPdfFormExample.pdf`);
+        const existingPdfBytes = fs.readFileSync(url);
+
+        const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
+        //cambiar desde aqui
+
+        const form = pdfDoc.getForm();
+
+        const field = form.getTextField("Given Name Text Box");
+        field.setText("Prueba pito");
+
+        //hasta aqui
+
+        const pdfFinal = await pdfDoc.saveAsBase64();
+
+        return res.status(200).json(ApiResponse(true, "PDF creado con exito", pdfFinal));
+    } catch (e) {
+        next(e);
+    }
+};
+
+module.exports = file;
--- src/routes/file.router.js
+++ src/routes/file.router.js
@@ -6,5 +6,6 @@ var {registerValidator, loginValidator} = require('../validators/user.validator'
 var fileController = require("../controllers/file.controller");
 
 router.get("/img/:imagen", fileController.getImg);
+router.post("/formularioSolicitud/obtener", fileController.obtenerPdfSolicitud);
 
 module.exports = router;
